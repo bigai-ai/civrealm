17ad82ff678b4eb637c2d38dcdaac87b96cebca0
chris1869 <Vincent_Vega2000@gmx.de>
2018-12-20 23:19:08 +0100

    bugfix7

--- src/freecivbot/build_server.py
+++ src/freecivbot/build_server.py
@@ -4,4 +4,5 @@ import docker
 
 def build_docker_img():
     client = docker.from_env()
+    print("Start building freeciv-web server. Take some coffee and relax. Takes up to 20minutes")
     client.images.build(path="https://github.com/chris1869/freeciv-web.git#develop", tag="freeciv-web")

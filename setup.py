822c7530e5798f7f79b4985cd921dbb139050f1a
chris1869 <Vincent_Vega2000@gmx.de>
2018-12-20 23:14:00 +0100

    bugfix7

--- setup.py
+++ setup.py
@@ -9,8 +9,9 @@ setup(name='freecivbot',
       license='GLP3.0',
       package_dir={'':'src'},
       packages=['freecivbot'],
+      extras_require={"DOCK_IMG" : []},
       entry_points = {
-        'console_scripts': ['create_img=freecivbot.build_server:build_docker_img'],
+        'console_scripts': ['create_img=freecivbot.build_server:build_docker_img [DOCK_IMG]'],
       },
       install_requires=['docker'],
       zip_safe=False)

02e58d40766184ab3f8a05d390e18c05d670644c
chris1869 <Vincent_Vega2000@gmx.de>
2018-12-20 22:42:42 +0100

    bugfix6

---
+++ src/freecivbot/build_server.py
@@ -0,0 +1,6 @@
+#Build freeciv-web from latest repo
+
+import docker
+
+client = docker.from_env()
+client.images.build(path="https://github.com/chris1869/freeciv-web.git#develop", tag="freeciv-web")

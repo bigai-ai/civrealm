fdc1b53eff624de10262f60fa2665fd90c9b4019
chris1869 <Vincent_Vega2000@gmx.de>
2018-12-20 23:35:25 +0100

    bugfix7

--- src/freecivbot/build_server.py
+++ src/freecivbot/build_server.py
@@ -1,8 +1,12 @@
 #Build freeciv-web from latest repo
-
 import docker
 
 def build_docker_img():
     client = docker.from_env()
     print("Start building freeciv-web server. Take some coffee and relax. Takes up to 20minutes")
-    client.images.build(path="https://github.com/chris1869/freeciv-web.git#develop", tag="freeciv-web")
+    cli = docker.APIClient(base_url='unix://var/run/docker.sock')
+    for line in cli.build(path="https://github.com/chris1869/freeciv-web.git#develop", tag="freeciv-web"):
+        if not "Downloading" in line:
+            print line
+
+build_docker_img()

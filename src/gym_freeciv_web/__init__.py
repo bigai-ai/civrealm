7bdeb19792dd1ae61b2fd9bef00a3a69cd488e42
chris1869 <Vincent_Vega2000@gmx.de>
2018-12-21 20:05:15 +0100

    OpenAI gym

--- src/gym_freeciv_web/__init__.py
+++ src/gym_freeciv_web/__init__.py
@@ -5,7 +5,7 @@ logger = logging.getLogger(__name__)
 
 register(
     id='Freeciv-v0',
-    entry_point='gym_freeciv.envs:FreecivEnv',
+    entry_point='gym_freeciv_web.envs:FreecivEnv',
     #timestep_limit=1000,
     #reward_threshold=1.0,
     #nondeterministic = True,

ded15c464db34a32fc993812d7f83c496dbbcf11
chris1869 <Vincent_Vega2000@gmx.de>
2019-01-01 18:59:11 +0100

    Bugfixing Selenium prep distribution

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(name='freecivbot',
       license='GLP3.0',
       package_dir=package_dirs,
       packages=package_list,
-      data_files = [('/usr/local/bin', ['civ_prep_selenium.sh'])]
+      data_files = [('/usr/local/bin', ['civ_prep_selenium.sh'])],
       entry_points = {'console_scripts': ["build_freeciv_server=freecivbot.build_server:build_docker_img",
                                           "test_freeciv_web_gym=gym_freeciv_web.random_test:main"]},
       install_requires=['docker','urllib3', 'BitVector', 'numpy', 'tornado', 'gym', 'selenium', 'websocket'],

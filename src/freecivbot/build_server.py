d23b190dd6c99572ebac1bb7e9eecd44bb731a34
chris1869 <Vincent_Vega2000@gmx.de>
2019-01-01 19:06:05 +0100

    bugfix selenium prep

--- src/freecivbot/build_server.py
+++ src/freecivbot/build_server.py
@@ -3,8 +3,6 @@ import docker
 import os
 
 def build_docker_img():
-    absp = os.path.abspath(__file__)
-    os.system("sudo " + os.path.dirname(absp) + os.sep + "prep_fire_selenium.sh")
     client = docker.from_env()
     print("Start building freeciv-web server. Take some coffee and relax. Takes up to 20minutes")
     cli = docker.APIClient(base_url='unix://var/run/docker.sock')

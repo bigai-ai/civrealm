682cf40da48a4344716bd96e278ec82b3eca5f25
weiwang-nlpr <wangwei@nlpr.ia.ac.cn>
2023-06-18 10:21:01 +0800

    revise log_file_path joining error

--- src/freecivbot/utils/freeciv_logging.py
+++ src/freecivbot/utils/freeciv_logging.py
@@ -4,6 +4,8 @@ import logging.config
 
 # You can set the level to logging.DEBUG or logging.WARN if you want to change the amount of output.
 log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '../../logging.conf')
+log_file_path = os.path.normpath(log_file_path)
+
 logging.config.fileConfig(log_file_path, disable_existing_loggers=False)
 logger = logging.getLogger('freecivGym')
 logger.setLevel(logging.DEBUG)

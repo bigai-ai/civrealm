b50ac4f645d7e4e1fd6994510ecae369eb4e6c3e
kongxiangyu <kongxiangyu@bigai.ai>
2023-07-13 10:36:01 +0800

    Add test for building road (basic version)

---
+++ tests/test_build_road.py
@@ -0,0 +1,84 @@
+# # Copyright (C) 2023  The Freeciv-gym project
+# #
+# # This program is free software: you can redistribute it and/or modify it
+# # under the terms of the GNU General Public License as published by the Free
+# #  Software Foundation, either version 3 of the License, or (at your option)
+# # any later version.
+# #
+# # This program is distributed in the hope that it will be useful, but
+# # WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
+# # or FITNESS FOR A PARsrc/freeciv_gym/configs/default_setting.ymlTICULAR PURPOSE.  See the GNU General Public License for more details.
+# #
+# # You should have received a copy of the GNU General Public License along
+# # with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import pytest
+from freeciv_gym.freeciv.civ_controller import CivController
+from freeciv_gym.freeciv.map.map_ctrl import DIR8_NORTH
+from freeciv_gym.freeciv.game.ruleset import EXTRA_ROAD
+
+@pytest.fixture
+def controller():
+    controller = CivController('testcontroller')
+    controller.set_parameter('debug.load_game', 'testcontroller_T27_2023-07-10-05_23')
+    yield controller
+    # Delete gamesave saved in handle_begin_turn
+    controller.handle_end_turn(None)
+    controller.close()
+
+def test_build_road(controller):
+    controller.init_network()
+    controller.get_observation()
+    options= controller.turn_manager.get_available_actions()
+    # Class: UnitActions
+    unit_opt = options['unit']    
+    test_action_list = []
+    build_action = None
+    worker_id = 138
+    for unit_id in unit_opt.unit_ctrl.units.keys():    
+        punit = unit_opt.unit_ctrl.units[unit_id]        
+        unit_tile = unit_opt.map_ctrl.index_to_tile(punit['tile'])
+        print(f"Unit id: {unit_id}, position: ({unit_tile['x']}, {unit_tile['y']}), move left: {unit_opt.unit_ctrl.get_unit_moves_left(punit)}.")        
+        # Get valid actions
+        valid_actions = unit_opt.get_actions(unit_id, valid_only=True)
+        if unit_id == worker_id:
+            test_action_list.append(valid_actions[f'goto_{DIR8_NORTH}'])
+            build_action = valid_actions['road']
+        else:
+            pass
+    print('Move to the north tile')
+    # Perform goto action for the worker
+    for action in test_action_list:
+        action.trigger_action(controller.ws_client)
+    # Get unit new state
+    controller.get_observation()
+    punit = unit_opt.unit_ctrl.units[worker_id]
+    build_tile = unit_opt.map_ctrl.index_to_tile(punit['tile'])
+    # The unit has no move left, the build should be invalid
+    assert(build_action is None or not build_action.is_action_valid())
+    # End turn
+    controller.send_end_turn()
+    controller.get_observation()
+    print(f"Unit id: {worker_id}, position: ({build_tile['x']}, {build_tile['y']}), extras[EXTRA_ROAD]: {build_tile['extras'][EXTRA_ROAD]}, move left: {unit_opt.unit_ctrl.get_unit_moves_left(punit)}.")
+    # The unit has move in new turn, the build should be valid
+    assert(not build_tile['extras'][EXTRA_ROAD] == 1)
+    assert(build_action.is_action_valid())
+    build_action.trigger_action(controller.ws_client)
+    print('Begin building a road, needs a few turns to finish ...')
+    # Wait for 15 turns (until the work is done)
+    for turn_i in range(5):
+        controller.send_end_turn()
+        controller.get_observation()
+    # Get updated state
+    print(f"Unit id: {worker_id}, position: ({build_tile['x']}, {build_tile['y']}), extras[EXTRA_ROAD]: {build_tile['extras'][EXTRA_ROAD]}, move left: {unit_opt.unit_ctrl.get_unit_moves_left(punit)}.")
+    assert(build_tile['extras'][EXTRA_ROAD] == 1)
+
+def main():
+    controller = CivController('testcontroller')
+    controller.set_parameter('debug.load_game', 'testcontroller_T27_2023-07-10-05_23')
+    test_build_road(controller)
+
+
+if __name__ == '__main__':
+    main()

fc514e2994aa40d8618e4ac3685987cebf31f327
kongxiangyu <kongxiangyu@bigai.ai>
2023-07-31 22:17:31 +0800

    Update build railroad test

--- tests/unit/test_build_railroad.py
+++ tests/unit/test_build_railroad.py
@@ -16,7 +16,7 @@
 
 import pytest
 from freeciv_gym.freeciv.civ_controller import CivController
-from freeciv_gym.freeciv.game.ruleset import EXTRA_RAILROAD
+from freeciv_gym.freeciv.game.ruleset import EXTRA_ROAD, EXTRA_RAILROAD
 import freeciv_gym.freeciv.map.map_const as map_const
 from freeciv_gym.freeciv.utils.freeciv_logging import fc_logger
 from freeciv_gym.configs import fc_args
@@ -39,37 +39,61 @@ def test_build_railroad(controller):
     _, options = get_first_observation_option(controller)
     # Class: UnitActions
     unit_opt = options['unit']
-    test_action_list = []
-    build_action = None
-    worker_id = 109
-    for unit_id in unit_opt.unit_ctrl.units.keys():
+    
+    unit_ids = [109, 236, 259]
+    for unit_id in unit_ids:
         punit = unit_opt.unit_ctrl.units[unit_id]
         unit_tile = unit_opt.map_ctrl.index_to_tile(punit['tile'])
-        # Get valid actions
-        valid_actions = unit_opt.get_actions(unit_id, valid_only=True)
-        if unit_id == worker_id:
-            build_action = valid_actions['railroad']
-            break
-        else:
-            pass
-    print(
+        print(
             f"Unit id: {unit_id}, position: ({unit_tile['x']}, {unit_tile['y']}), move left: {unit_opt.unit_ctrl.get_unit_moves_left(punit)}.")
-    build_tile = unit_opt.map_ctrl.index_to_tile(punit['tile'])
-    print(
-        f"Unit id: {worker_id}, position: ({build_tile['x']}, {build_tile['y']}), extras[EXTRA_RAILROAD]: {build_tile['extras'][EXTRA_RAILROAD]}, move left: {unit_opt.unit_ctrl.get_unit_moves_left(punit)}.")
-    # The unit has move in new turn, the build should be valid
-    assert (not build_tile['extras'][EXTRA_RAILROAD] == 1)
-    assert (build_action.is_action_valid())
-    build_action.trigger_action(controller.ws_client)
-    print('Begin building a railroad, needs a few turns to finish ...')
-    # Wait for 5 turns (until the work is done)
+        build_tile = unit_opt.map_ctrl.index_to_tile(punit['tile'])
+        valid_actions = unit_opt.get_actions(unit_id, valid_only=True)
+
+        if unit_id == 109:
+            assert (build_tile['extras'][EXTRA_ROAD] == 1)
+            assert (not build_tile['extras'][EXTRA_RAILROAD] == 1)
+            # worker on a tile with road, should be able to build railroad
+            print(
+                f"Unit id: {unit_id}, position: ({build_tile['x']}, {build_tile['y']}), extras[EXTRA_RAILROAD]: {build_tile['extras'][EXTRA_RAILROAD]}, move left: {unit_opt.unit_ctrl.get_unit_moves_left(punit)}.")
+            assert ('railroad' in valid_actions)
+            print(f'unit_id: {unit_id} build railroad.')
+            valid_actions['railroad'].trigger_action(controller.ws_client)
+        if unit_id == 236:
+            # worker on a tile without road, shouldn't be able to build railroad
+            assert (not build_tile['extras'][EXTRA_ROAD] == 1)
+            assert ('railroad' not in valid_actions)
+        if unit_id == 259:
+            # non-worker unit, shouldn't be able to build railroad
+            # cancel fortify orders
+            valid_actions['cancel_order'].trigger_action(controller.ws_client)
+            controller.get_info()
+            controller.get_observation()
+            valid_actions = unit_opt.get_actions(unit_id, valid_only=True)
+            # move to northeast tile which has road
+            valid_actions[f'goto_{map_const.DIR8_NORTHEAST}'].trigger_action(controller.ws_client)
+    controller.get_info()
+    controller.get_observation()
+    for unit_id in unit_ids:
+        punit = unit_opt.unit_ctrl.units[unit_id]
+        build_tile = unit_opt.map_ctrl.index_to_tile(punit['tile'])
+        valid_actions = unit_opt.get_actions(unit_id, valid_only=True)
+        if unit_id == 259:
+            assert (build_tile['extras'][EXTRA_ROAD] == 1)
+            assert ('railroad' not in valid_actions)
+
+    # wait until railroad is built
     for turn_i in range(3):
         controller.send_end_turn()
+        controller.get_info()
         controller.get_observation()
-    # Get updated state
-    print(
-        f"Unit id: {worker_id}, position: ({build_tile['x']}, {build_tile['y']}), extras[EXTRA_RAILROAD]: {build_tile['extras'][EXTRA_RAILROAD]}, move left: {unit_opt.unit_ctrl.get_unit_moves_left(punit)}.")
-    assert (build_tile['extras'][EXTRA_RAILROAD] == 1)
+    for unit_id in unit_ids:
+        punit = unit_opt.unit_ctrl.units[unit_id]
+        build_tile = unit_opt.map_ctrl.index_to_tile(punit['tile'])
+        if unit_id == 109:
+            print(
+                f"Unit id: {unit_id}, position: ({build_tile['x']}, {build_tile['y']}), extras[EXTRA_RAILROAD]: {build_tile['extras'][EXTRA_RAILROAD]}, move left: {unit_opt.unit_ctrl.get_unit_moves_left(punit)}.")
+            assert (build_tile['extras'][EXTRA_RAILROAD] == 1)
+
     import time
     time.sleep(2)
 

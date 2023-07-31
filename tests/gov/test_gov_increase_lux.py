c277877bf2747e8850391219c63e98b7c0360aab
liyx <liyexin@bigai.ai>
2023-07-31 17:47:45 +0800

    add test_gov_increase_lux and test_gov_decrease_lux

---
+++ tests/gov/test_gov_increase_lux.py
@@ -0,0 +1,60 @@
+# # Copyright (C) 2023  The Freeciv-gym project
+# #
+# # This program is free software: you can redistribute it and/or modify it
+# # under the terms of the GNU General Public License as published by the Free
+# #  Software Foundation, either version 3 of the License, or (at your option)
+# # any later version.
+# #
+# # This program is distributed in the hope that it will be useful, but
+# # WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
+# # or FITNESS FOR A PARsrc/freeciv_gym/configs/default_setting.ymlTICULAR PURPOSE.  See the GNU General Public License for more details.
+# #
+# # You should have received a copy of the GNU General Public License along
+# # with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import pytest
+import random
+from freeciv_gym.freeciv.civ_controller import CivController
+import freeciv_gym.freeciv.map.map_const as map_const
+from freeciv_gym.freeciv.utils.freeciv_logging import fc_logger
+from freeciv_gym.configs import fc_args
+from freeciv_gym.freeciv.utils.test_utils import get_first_observation_option
+
+@pytest.fixture
+def controller():
+    controller = CivController('testcontroller')
+    controller.set_parameter('debug.load_game', 'testcontroller_T31_2023-07-31-09_38')
+    yield controller
+    # Delete gamesave saved in handle_begin_turn
+    controller.handle_end_turn(None)
+    controller.end_game()
+    controller.close()
+
+
+def find_keys_with_keyword(dictionary, keyword):
+    keys = []
+    for key in dictionary:
+        if keyword in key:
+            keys.append(dictionary[key])
+    return keys
+
+
+def test_gov_increase_lux(controller):
+    fc_logger.info("test_gov_increase_lux")
+    _, options = get_first_observation_option(controller)
+
+    player_opt = options['player']
+    pplayer = player_opt.players[0]
+
+    increase_lux_action = find_keys_with_keyword(player_opt._action_dict[0], 'increase_lux')[0]
+    assert (increase_lux_action.is_action_valid())
+
+    lux_1 = pplayer['luxury']
+
+    increase_lux_action.trigger_action(controller.ws_client)
+    controller.get_observation()
+    lux_2 = pplayer['luxury']
+
+    assert (lux_2 - lux_1 == 10)
+
+

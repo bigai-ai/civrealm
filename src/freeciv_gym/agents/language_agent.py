bef87de086fc9e1bc6e8c6587dcae13c642946c2
liyx <liyexin@bigai.ai>
2023-08-11 16:28:21 +0800

    add get_actor_action to transform action returned by language model to action class

--- src/freeciv_gym/agents/language_agent.py
+++ src/freeciv_gym/agents/language_agent.py
@@ -55,55 +55,10 @@ DIR = [(0, 0), (0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, 
        (0, 2), (1, 2), (-1, 2), (2, 2), (-2, 2), (0, -2), (1, -2), (-1, -2), (2, -2),
        (-2, -2), (2, 0), (2, 1), (2, -1), (-2, 0), (-2, 1), (-2, -1)]
 
-MOVE_NAMES = {'goto_0': 'move NorthWest', 'goto_1': 'move North',
-              'goto_2': 'move NorthEast', 'goto_3': 'move West',
-              'goto_4': 'move East', 'goto_5': 'move SouthWest',
+MOVE_NAMES = {'goto_0': 'move NorthWest', 'goto_1': 'move North', 'goto_2': 'move NorthEast',
+              'goto_3': 'move West', 'goto_4': 'move East', 'goto_5': 'move SouthWest',
               'goto_6': 'move South', 'goto_7': 'move SouthEast'}
-
-'''
-current prompt examples:
-tile_info = {'current_tile': ['Forest', '1 Explorer'],
-             'tile_north_1': ['Tundra', 'Road', '1 Warriors'],
-             'tile_south_1': ['Plains', 'Buffalo'],
-             'tile_east_1': ['Mountains'],
-             'tile_west_1': ['Hills'],
-             'tile_north_1_east_1': ['Swamp', '1 Warriors'],
-             'tile_north_1_west_1': ['Forest'],
-             'tile_south_1_east_1': ['Forest', '1 Workers'],
-             'tile_south_1_west_1': ['Plains'],
-             'tile_north_2': ['Mountains', '1 Workers'],
-             'tile_north_2_east_1': ['Swamp'],
-             'tile_north_2_west_1': ['Swamp'],
-             'tile_north_2_east_2': ['Hills'],
-             'tile_north_2_west_2': ['Plains'],
-             'tile_south_2': ['Mountains'],
-             'tile_south_2_east_1': ['Grassland'],
-             'tile_south_2_west_1': ['Hills'],
-             'tile_south_2_east_2': ['Forest'],
-             'tile_south_2_west_2': ['Grassland'],
-             'tile_east_2': ['Grassland'],
-             'tile_north_1_east_2': ['Hills'],
-             'tile_south_1_east_2': ['Forest', 'Pheasant'],
-             'tile_west_2': ['Forest'],
-             'tile_north_1_west_2': ['Desert'],
-             'tile_south_1_west_2': ['Plains']
-             }
-
-unit_dict = {'Settlers 101': {'max_move': 0, 'avail_actions': []}, 
-             'Workers 103': {'max_move': 3, 'avail_actions': ['disband', 'keep_activity', 'move NorthWest', 
-                                                              'move North', 'move NorthEast', 'move West', 
-                                                              'move East', 'move SouthWest', 'move South', 
-                                                              'move SouthEast']}, 
-             'Workers 104': {'max_move': 3, 'avail_actions': ['disband', 'keep_activity', 'move NorthWest', 
-                                                              'move North', 'move NorthEast', 'move West', 
-                                                              'move East', 'move SouthWest', 'move South', 
-                                                              'move SouthEast']}, 
-             'Explorer 105': {'max_move': 3, 'avail_actions': ['disband', 'keep_activity', 'explore', 'fortify', 
-                                                               'move NorthWest', 'move North', 'move NorthEast', 
-                                                               'move West', 'move East', 'move SouthWest', 
-                                                               'move South', 'move SouthEast']}
-             }
-'''
+INVERSE_MOVE_NAMES = {val: key for key, val in MOVE_NAMES.items()}
 
 
 class LanguageAgent(ControllerAgent):
@@ -209,4 +164,60 @@ class LanguageAgent(ControllerAgent):
                 punit_name = UNIT_TYPES[punit]
                 unit_str.append(str(int(punit_number)) + ' ' + punit_name)
         return unit_str
+
+    def get_actor_action(self, info, ctrl_type, actor_id, action_name):
+        valid_action_dict = self.get_valid_actions(info, ctrl_type, actor_id)
+        if action_name in INVERSE_MOVE_NAMES:
+            action_name = INVERSE_MOVE_NAMES[action_name]
+
+        if action_name in valid_action_dict:
+            return valid_action_dict[action_name]
+        else:
+            raise Exception("Invalid Action !")
+
+
+'''
+tile_info = {'current_tile': ['Forest', '1 Explorer'],
+             'tile_north_1': ['Tundra', 'Road', '1 Warriors'],
+             'tile_south_1': ['Plains', 'Buffalo'],
+             'tile_east_1': ['Mountains'],
+             'tile_west_1': ['Hills'],
+             'tile_north_1_east_1': ['Swamp', '1 Warriors'],
+             'tile_north_1_west_1': ['Forest'],
+             'tile_south_1_east_1': ['Forest', '1 Workers'],
+             'tile_south_1_west_1': ['Plains'],
+             'tile_north_2': ['Mountains', '1 Workers'],
+             'tile_north_2_east_1': ['Swamp'],
+             'tile_north_2_west_1': ['Swamp'],
+             'tile_north_2_east_2': ['Hills'],
+             'tile_north_2_west_2': ['Plains'],
+             'tile_south_2': ['Mountains'],
+             'tile_south_2_east_1': ['Grassland'],
+             'tile_south_2_west_1': ['Hills'],
+             'tile_south_2_east_2': ['Forest'],
+             'tile_south_2_west_2': ['Grassland'],
+             'tile_east_2': ['Grassland'],
+             'tile_north_1_east_2': ['Hills'],
+             'tile_south_1_east_2': ['Forest', 'Pheasant'],
+             'tile_west_2': ['Forest'],
+             'tile_north_1_west_2': ['Desert'],
+             'tile_south_1_west_2': ['Plains']
+             }
+
+unit_dict = {'Settlers 101': {'max_move': 0, 'avail_actions': []}, 
+             'Workers 103': {'max_move': 3, 'avail_actions': ['disband', 'keep_activity', 'move NorthWest', 
+                                                              'move North', 'move NorthEast', 'move West', 
+                                                              'move East', 'move SouthWest', 'move South', 
+                                                              'move SouthEast']}, 
+             'Workers 104': {'max_move': 3, 'avail_actions': ['disband', 'keep_activity', 'move NorthWest', 
+                                                              'move North', 'move NorthEast', 'move West', 
+                                                              'move East', 'move SouthWest', 'move South', 
+                                                              'move SouthEast']}, 
+             'Explorer 105': {'max_move': 3, 'avail_actions': ['disband', 'keep_activity', 'explore', 'fortify', 
+                                                               'move NorthWest', 'move North', 'move NorthEast', 
+                                                               'move West', 'move East', 'move SouthWest', 
+                                                               'move South', 'move SouthEast']}
+             }
+'''
+
 

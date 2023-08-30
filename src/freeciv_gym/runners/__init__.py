04507e67c6bd2f45ef7676d60946f8d9d89fa603
Siyuan Qi <syqi@bigai.ai>
2023-08-30 17:45:03 +0800

    Add checking for batch_size_run.

--- src/freeciv_gym/runners/__init__.py
+++ src/freeciv_gym/runners/__init__.py
@@ -1,7 +1,1 @@
-REGISTRY = {}
-
-# from .episode_runner import EpisodeRunner
-# REGISTRY["episode"] = EpisodeRunner
-
 from .parallel_runner import ParallelRunner
-REGISTRY["parallel"] = ParallelRunner

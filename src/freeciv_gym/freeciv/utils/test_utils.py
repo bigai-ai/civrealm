1e3fca1f529bb4bec8e1995ba6414c9caf48714a
Siyuan Qi <syqi@bigai.ai>
2023-08-30 11:10:01 +0800

    Update test_utils to get turn options.

--- src/freeciv_gym/freeciv/utils/test_utils.py
+++ src/freeciv_gym/freeciv/utils/test_utils.py
@@ -2,9 +2,10 @@ import time
 import random
 from freeciv_gym.freeciv.utils.freeciv_logging import fc_logger
 from freeciv_gym.freeciv.utils.port_list import DEV_PORT_LIST
+from freeciv_gym.freeciv.civ_controller import CivController
 
 
-def get_first_observation_option(controller, client_port=None):
+def get_first_observation_option(controller: CivController, client_port=None):
     # Random choose one port for test
     if client_port is not None:
         controller.client_port = client_port
@@ -12,18 +13,16 @@ def get_first_observation_option(controller, client_port=None):
         controller.client_port = random.choice(DEV_PORT_LIST)
     # Reset controller. Otherwise, the states (clstate, conn info, etc.) changed in the previous login will cause errors.
     controller.reset()
-    opt = None
-    obs = None
     # Handle port conflict if exist
     try:
         controller.init_network()
-        opt = controller.get_info()['available_actions']
-        obs = controller.get_observation()
+        controller.get_info()
+        options = controller.turn_manager.turn_actions
+        observation = controller.get_observation()
         # controller.clstate.set_multiplayer_game()
+        return observation, options
     except Exception as e:
         fc_logger.error(f'get_first_observation_option error: {repr(e)}')
         # Sleep for a random time and try again
         time.sleep(random.uniform(3, 5))
-        obs, opt = get_first_observation_option(controller, client_port)
-
-    return obs, opt
+        return get_first_observation_option(controller, client_port)

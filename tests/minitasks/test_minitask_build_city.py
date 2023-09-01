f0dbcb324a4faddfca675c313069f0626553dd12
Siyuan Qi <syqi@bigai.ai>
2023-09-01 19:27:20 +0800

    Remove double comments in license.

--- tests/minitasks/test_minitask_build_city.py
+++ tests/minitasks/test_minitask_build_city.py
@@ -1,17 +1,17 @@
-# # Copyright (C) 2023  The Freeciv-gym project
-# #
-# # This program is free software: you can redistribute it and/or modify it
-# # under the terms of the GNU General Public License as published by the Free
-# # Software Foundation, either version 3 of the License, or (at your option)
-# # any later version.
-# #
-# # This program is distributed in the hope that it will be useful, but
-# # WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
-# # or FITNESS FOR A PARsrc/freeciv_gym/configs/default_setting.ymlTICULAR PURPOSE.  See the GNU General Public License
+# Copyright (C) 2023  The Freeciv-gym project
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
 # for more details.
-# #
-# # You should have received a copy of the GNU General Public License along
-# # with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import time
 import random
@@ -23,6 +23,7 @@ from freeciv_gym.freeciv.utils.test_utils import get_first_observation_option
 import freeciv_gym.freeciv.utils.fc_types as fc_types
 from freeciv_gym.configs.logging_config import LOGGING_CONFIG
 from freeciv_gym.freeciv.build_server import run_bash_command
+
 
 def test_build_city(controller):
     # configure_test_logger()
@@ -44,7 +45,7 @@ def test_build_city(controller):
         info, observation = controller.get_info_and_observation()
         print("info: ", info)
         print("observation: ", observation)
-        #unit_opt = options['unit']
+        # unit_opt = options['unit']
         steps += 1
         print("get_info_and_observation wasted time: ", time.time() - start)
         controller.send_end_turn()
@@ -52,9 +53,11 @@ def test_build_city(controller):
         controller.handle_end_turn(None)
         print("handle_end_turn wasted time: ", time.time() - start)
 
+
 def run_command(cmd):
     pi = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
     return [sav.strip().split(".sav")[0] for sav in pi.stdout.read().decode("utf8").strip().split("\n")]
+
 
 def get_minitask(name, docker_image='freeciv-web', docker_sav_path='/var/lib/tomcat10/webapps/data/savegames/'):
     """ Get Minitask Sav File. """
@@ -64,13 +67,15 @@ def get_minitask(name, docker_image='freeciv-web', docker_sav_path='/var/lib/tom
     print(f"GETTING {task_size} MINITASKS FOR {name},  {minitask} IS CHOSEN!")
     return minitask
 
+
 def main(username):
-    fc_args['username'] = "myagent" #username
+    fc_args['username'] = "myagent"  # username
     controller = CivController(fc_args['username'])
-    controller.set_parameter('debug.load_game', 'myagent_T1_task_build_city')#get_minitask(fc_args['username'])
+    controller.set_parameter('debug.load_game', 'myagent_T1_task_build_city')  # get_minitask(fc_args['username'])
     test_build_city(controller)
-    #controller.send_end_turn()
-    #controller.handle_end_turn(None)
+    # controller.send_end_turn()
+    # controller.handle_end_turn(None)
+
 
 if __name__ == '__main__':
     main('minitaskbuildcity')

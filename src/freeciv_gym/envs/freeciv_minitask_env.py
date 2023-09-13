d43a6772810eb3ab5be49357ac254bbc8a2f1bb8
huangping <huangping@bigai.ai>
2023-09-13 16:02:51 +0800

    add MinitaskGameStatus & MinitaskPlayerStatus & _get_reward/success/status/detail & put minitask data into info of step

--- src/freeciv_gym/envs/freeciv_minitask_env.py
+++ src/freeciv_gym/envs/freeciv_minitask_env.py
@@ -44,6 +44,18 @@ class MinitaskType(ExtendedEnum):
     MT_ATTACK_CITY = "attackcity"
     MT_DEFEND_CITY = "defendcity"
     MT_TRADE_TECH = "tradetech"
+    MT_DEVELOPMENT = "development"
+
+@unique
+class MinitaskGameStatus(ExtendedEnum):
+    MGS_END_GAME = 1
+    MGS_IN_GAME = 0
+
+@unique
+class MinitaskPlayerStatus(ExtendedEnum):
+    MPS_SUCCESS = 1
+    MPS_FAIL = 0
+    MPS_UNKNOWN = -1
 
 class FreecivMinitaskEnv(FreecivBaseEnv):
     """ Freeciv gym environment for minitasks. """
@@ -55,6 +67,7 @@ class FreecivMinitaskEnv(FreecivBaseEnv):
         self.filename = None
         self.task_type = None
         fc_args['debug.autosave'] = False
+        self._last_minitask_score = 0
 
     @staticmethod
     def get_minitask(name, 
@@ -96,12 +109,55 @@ class FreecivMinitaskEnv(FreecivBaseEnv):
     def minitask_has_terminated(self):
         """ Judge whether the minitask is terminated. """
         minitask_info = self.civ_controller.get_turn_message()
-        if any([msg.get("status") for msg in minitask_info]):
+        if any([msg.get("status") == MinitaskGameStatus.MGS_END_GAME.value for msg in minitask_info]):
             return True
         return False
-
+    
     def _get_terminated(self):
         return self.civ_controller.game_has_terminated() or self.minitask_has_terminated()
+
+    def _get_reward(self):
+        minitask_results = self.civ_controller.get_turn_message()
+        current_score = 0.0
+        for msg in minitask_results[::-1]:
+            if 'metrics' in msg and 'mini_score' in msg['metrics'][-1]:
+                current_score = msg['metrics'][-1]['mini_score'] - self._last_minitask_score
+                self._last_minitask_score = msg['metrics'][-1]['mini_score']
+        return current_score
+
+    def _get_game_status(self):
+        minitask_results = self.civ_controller.get_turn_message()
+        for msg in minitask_results[::-1]:
+            if 'status' in msg:
+                return msg['status']
+        return MinitaskGameStatus.MGS_IN_GAME.value
+
+    def _get_success(self):
+        minitask_results = self.civ_controller.get_turn_message()
+        for msg in minitask_results[::-1]:
+            if 'metrics' in msg and 'is_minitask_success' in msg['metrics'][-1]:
+                return msg['metrics'][-1]['is_minitask_success']
+        return MinitaskPlayerStatus.MPS_UNKNOWN.value
+
+    def _get_detail(self):
+        minitask_results = self.civ_controller.get_turn_message()
+        detail = dict()
+        for msg in minitask_results[::-1]:
+            if 'metrics' in msg:
+                if 'mini_goal' in msg['metrics'][-1]:
+                    detail['goal'] = msg['metrics'][-1]['mini_goal']
+                if 'max_turn' in msg['metrics'][-1]:
+                    detail['max_turn'] = msg['metrics'][-1]['max_turn']
+        return detail
+
+    def step(self, action):
+        observation, reward, terminated, truncated, info = super().step(action)
+        info['minitask'] = {
+            'status': self._get_game_status(),
+            'success': self._get_success(),
+        }
+        info['minitask'].update(self._get_detail())
+        return observation, reward, terminated, truncated, info
 
     def get_game_results(self):
         """ Merge game result and minitask. """

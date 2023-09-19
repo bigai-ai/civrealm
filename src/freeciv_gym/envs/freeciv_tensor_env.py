7c6c5327ca8e6e30b32c3cf5d718b83572721e42
yangbangcheng <yangbangcheng@bigai.ai>
2023-09-19 15:39:43 +0800

    Env: change tensor env turn done penalty from -1 to -0.1

--- src/freeciv_gym/envs/freeciv_tensor_env.py
+++ src/freeciv_gym/envs/freeciv_tensor_env.py
@@ -14,7 +14,8 @@ from gymnasium.core import Wrapper
 
 from freeciv_gym.configs import fc_args
 from freeciv_gym.envs.freeciv_base_env import FreecivBaseEnv
-from freeciv_gym.envs.freeciv_wrapper import TensorWrapper, PenalizeTurnDoneReward
+from freeciv_gym.envs.freeciv_wrapper import (PenalizeTurnDoneReward,
+                                              TensorWrapper)
 from freeciv_gym.envs.freeciv_wrapper.utils import default_tensor_config
 
 
@@ -30,7 +31,9 @@ class FreecivTensorEnv(Wrapper):
         config: dict = default_tensor_config,
     ):
         tensor_env = TensorWrapper(
-            env=PenalizeTurnDoneReward(FreecivBaseEnv(username=username, client_port=client_port)),
+            env=PenalizeTurnDoneReward(
+                FreecivBaseEnv(username=username, client_port=client_port), penalty=-0.1
+            ),
             config=config,
         )
         super().__init__(tensor_env)

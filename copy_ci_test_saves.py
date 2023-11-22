67f829fdf2f066d6ef5f34ab87d7588171ad3772
yangbangcheng <yangbangcheng@bigai.ai>
2023-11-22 13:52:52 +0800

    CI: update .gitlab-ci.yml; fix typo

--- copy_ci_test_saves.py
+++ copy_ci_test_saves.py
@@ -8,12 +8,12 @@ def copy_test_game_save():
         SAVE_DIR = os.environ["CIVREALM_TEST_SAVE_DIR"]
         print(f"Using game_save dir specified by SAVE_DIR: {SAVE_DIR}")
     else:
-        SAVE_DIR = "/builds$/civilization/civrealm/tests/game_save"
+        SAVE_DIR = "/builds/civilization/civrealm/tests/game_save"
         print(f"SAVE_DIR not found, using default game_save dir: {SAVE_DIR}")
 
     for username in next(os.walk(SAVE_DIR))[1]:
         subprocess.call(
-            f"cp -rd {test_dir}/game_save/{username} /var/lib/{tomcat_dir}/webapps/data/savegames/",
+            f"cp -rd {SAVE_DIR}/{username} /var/lib/tomcat10/webapps/data/savegames/",
             shell=True,
             executable="/bin/bash",
         )
